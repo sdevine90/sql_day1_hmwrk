4a119f8c6d3808fd4aae08461c54d2cfeaadcdab
Stephanie <s.devine90@gmail.com>
2016-11-14 20:00:00 +0000

    updated

--- marvel.sql
+++ marvel.sql
@@ -45,10 +45,13 @@ INSERT INTO movies (title, year, show_time)
 VALUES ('Thor: The Dark World', 2013, '18:55');
 
 INSERT INTO movies (title, year, show_time) 
+VALUES ('Batman Begins', 2005, '16:30');
+
+INSERT INTO movies (title, year, show_time) 
 VALUES ('Captain America: The Winter Soldier', 2014, '19:20');
 
 INSERT INTO movies (title, year, show_time) 
-VALUES ('Guardians of the Galaxy', 2014, '21:30');
+VALUES ('Guardians of the Galaxy', 2014, '12:10');
 
 INSERT INTO movies (title, year, show_time) 
 VALUES ('Avengers: Age of Ultron', 2015, '23:40');
@@ -62,14 +65,8 @@ VALUES ('Captain America: Civil War', 2016, '22:00');
 INSERT INTO movies (title, year, show_time) 
 VALUES ('Doctor Strange', 2016, '16:30');
 
-INSERT INTO movies (title, year, show_time)
-VALUES ('Guardians of the Galaxy 2', 2016, '24:00'); 
-
-
-
-
-
-
+INSERT INTO movies (title, year, show_time) 
+VALUES ('Guardian of the Galaxy 2', 2016, '24:00');
 
 INSERT INTO people (name) VALUES ('Chris Bacon');
 INSERT INTO people (name) VALUES ('John Campbell');
@@ -81,15 +78,36 @@ INSERT INTO people (name) VALUES ('Cameron Fulton');
 INSERT INTO people (name) VALUES ('Tegan Gallacher');
 INSERT INTO people (name) VALUES ('Gregor Gilchrist');
 INSERT INTO people (name) VALUES ('Claire Hilditch');
+INSERT INTO people (name) VALUES ('Graeme Bell');
 INSERT INTO people (name) VALUES ('Winnie Ho');
-INSERT INTO people (name) VALUES ('Jeff 3');
+INSERT INTO people (name) VALUES ('Adam Leel');
 INSERT INTO people (name) VALUES ('Lewis MacNee');
-INSERT INTO people (name) VALUES ('Jeff 3.2');
+INSERT INTO people (name) VALUES ('Adam Nattrass');
 INSERT INTO people (name) VALUES ('William Robertson');
 INSERT INTO people (name) VALUES ('Jordan Said');
 INSERT INTO people (name) VALUES ('Logan Smith');
 INSERT INTO people (name) VALUES ('Frederico Zucca');
 INSERT INTO people (name) VALUES ('Val Dryden');
+
+DELETE FROM movies
+WHERE title = 'Batman Begins';
+
+DELETE FROM people
+WHERE name = 'Graeme Bell';
+
+UPDATE people
+SET name = 'Jeff 3'
+WHERE name = 'Adam Nattrass';
+
+UPDATE people
+SET name = 'Jeff 3.2'
+WHERE name = 'Adam Leel';
+
+UPDATE movies
+SET show_time = '21:30'
+WHERE show_time = '12:10';
+
+
 
 SELECT * FROM movies;
 SELECT name FROM people WHERE name = 'Stephanie Devine';
